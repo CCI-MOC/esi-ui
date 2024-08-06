36a263004181e03427174d2673df4f7fafe1053f
Austin Jamias <ajamias@redhat.com>
2024-08-06 04:17:54 -0400

    Add Baremetal Topology Panel
    In the dashboard, if you click on Network > Network Topology, it
    shows a graph topology of the networks and instances. This panel
    does not work for baremetal nodes which ESI uses. This adds a
    topology view of the baremetal nodes and their MAC address
    connections to network ports in addition to the already existing
    network topology.

---
+++ esi_ui/enabled/_6030_project_network_topology_panel.py
@@ -0,0 +1,27 @@
+# The name of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'baremetal_topology'
+
+# The name of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'project'
+PANEL_GROUP = 'esi'
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL = 'esi_ui.content.baremetal_topology.panel.BaremetalTopology'
+
+# A list of applications to be prepended to INSTALLED_APPS
+#ADD_INSTALLED_APPS = ['esi_ui']
+
+# A list of AngularJS modules to be loaded when Angular bootstraps.
+#ADD_ANGULAR_MODULES = ['horizon.dashboard.project.esi']
+
+# Automatically discover static resources in installed apps
+AUTO_DISCOVER_STATIC_FILES = True
+
+# A list of js files to be included in the compressed set of files
+#ADD_JS_FILES = []
+
+# A list of scss files to be included in the compressed set of files
+#ADD_SCSS_FILES = ['dashboard/identity/myplugin/mypanel/mypanel.scss']
+
+# A list of template-based views to be added to the header
+#ADD_HEADER_SECTIONS = ['esi_ui.content.esi_nodes.views.HeaderView',]
